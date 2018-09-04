ed097c71eaf223a581be0f494ab5bb2a13f487ff
moka3660 <moka12almukha30@gmail.com>
2018-09-04 13:20:56 +0900

    update OhajikiSW.ino
    1)入出力設定

--- OhajikiSW/OhajikiSW.ino
+++ OhajikiSW/OhajikiSW.ino
@@ -5,7 +5,11 @@ const int LED = 1;//for debug
 
 void setup()
 {
-
+  //入出力設定
+  pinMode(PHOTO_SW,INPUT);
+  pinMode(VIB_SW,INPUT);
+  pinMode(LED_SIG,OUTPUT);
+  pinMode(LED,OUTPUT);
 }
 
 void loop()
