1bf1a5ed9cc1b37d1ae657fe2d43a8c63a0adf53
moka3660 <moka12almukha30@gmail.com>
2018-09-04 16:23:49 +0900

    comment OhajikiSW.ino

--- OhajikiSW/OhajikiSW.ino
+++ OhajikiSW/OhajikiSW.ino
@@ -26,13 +26,15 @@ void loop()
       break;
   }
 
+  //衝撃検知
   boolean vibstate = false;
   int chattercount = 0;
   unsigned long vibtime;
   unsigned long chattertime;
-  vibtime = millis();
+  vibtime = millis(); //開始時間取得
   while(millis()-vibtime<vibLimit)
   {
+    //チャタリングカウンタ
     if(vibstate != digitalRead(VIB_SW))
     {
       vibstate = !vibstate;
@@ -40,11 +42,13 @@ void loop()
       if(chattercount==1)
         chattertime = millis();
     }
+    //チャタリング回数が一定以上になればモード変更して脱出
     if(chattercount >= chatterTimes)
     {
       mode = true;
       break;
     }
+    //チャタリング回数に達しなければカウンタ初期化
     if(millis()-chattertime > chatterLimit)
       chattercount = 0;
   }
