d50014381d0f1b5e1a724ee0f20347a44c9c607a
shibain33 <rozenzulu3660@gmail.com>
2018-10-16 07:53:58 +0900

    update LEDtest
    1)LED指定用ピン追加

--- LEDtest_UNO/LEDtest_UNO.ino
+++ LEDtest_UNO/LEDtest_UNO.ino
@@ -1,9 +1,18 @@
 const int LED_SIG = 0;
+const int LED_R = 1;
+const int LED_G = 2;
+const int LED_B = 5;
 
 void setup()
 {
   pinMode(LED_SIG, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT);
   digitalWrite(LED_SIG, LOW);
+  digitalWrite(LED_R,LOW);
+  digitalWrite(LED_G,LOW);
+  digitalWrite(LED_B,LOW);
 }
 
 void loop()
