9730cb7c6cbd4398fadba85c353852574ae0824b
moka3660 <moka12almukha30@gmail.com>
2018-10-17 21:39:53 +0900

    update(fix) LEDtest
    ダイナミックだから点灯し続けるだけでもループいるよね  <-修正

--- LEDtest_UNO/LEDtest_UNO.ino
+++ LEDtest_UNO/LEDtest_UNO.ino
@@ -3,6 +3,9 @@ const int LED_R = 1;
 const int LED_G = 2;
 const int LED_B = 5;
 
+#define colorlength 1000
+unsigned long colortimes;
+
 void setup()
 {
   pinMode(LED_SIG, OUTPUT);
@@ -17,28 +20,34 @@ void setup()
 
 void loop()
 {
-  digitalWrite(LED_R, HIGH);
-  analogWrite(LED_SIG, 126);
-  digitalWrite(LED_R, LOW);
-  digitalWrite(LED_G, HIGH);
-  analogWrite(LED_SIG, 126);
-  digitalWrite(LED_G, LOW);
-  digitalWrite(LED_B, HIGH);
-  analogWrite(LED_SIG, 126);
-  digitalWrite(LED_B, LOW);
-  digitalWrite(LED_R, HIGH);
+  colortimes = millies();
+  while(millis()-colortimes < colorlength)
+  {
+    digitalWrite(LED_R, HIGH);
+    analogWrite(LED_SIG, 126);
+    digitalWrite(LED_R, LOW);
+    digitalWrite(LED_G, HIGH);
+    analogWrite(LED_SIG, 126);
+    digitalWrite(LED_G, LOW);
+    digitalWrite(LED_B, HIGH);
+    analogWrite(LED_SIG, 126);
+    digitalWrite(LED_B, LOW);
+    digitalWrite(LED_R, HIGH);
+  }
 
-  delay(1000);
-
-  digitalWrite(LED_R, HIGH);
-  analogWrite(LED_SIG, 255);
-  digitalWrite(LED_R, LOW);
-  digitalWrite(LED_G, HIGH);
-  analogWrite(LED_SIG, 0);
-  digitalWrite(LED_G, LOW);
-  digitalWrite(LED_B, HIGH);
-  analogWrite(LED_SIG, 0);
-  digitalWrite(LED_B, LOW);
-  digitalWrite(LED_R, HIGH);
-
+//  delay(1000);
+  colortimes = millies();
+  while(millis()-colortimes < colorlength)
+  {
+    digitalWrite(LED_R, HIGH);
+    analogWrite(LED_SIG, 255);
+    digitalWrite(LED_R, LOW);
+    digitalWrite(LED_G, HIGH);
+    analogWrite(LED_SIG, 0);
+    digitalWrite(LED_G, LOW);
+    digitalWrite(LED_B, HIGH);
+    analogWrite(LED_SIG, 0);
+    digitalWrite(LED_B, LOW);
+    digitalWrite(LED_R, HIGH);
+  }
 }
