9ca4eceae43256ab61e8d2d706ef824acf43f286
moka3660 <moka12almukha30@gmail.com>
2018-10-17 21:47:20 +0900

    clean OhajikiSW
    1)define名称の変更(Limit->Length)
    2)スペース追加で見栄え良く

--- OhajikiSW/OhajikiSW.ino
+++ OhajikiSW/OhajikiSW.ino
@@ -4,21 +4,21 @@ const int LED_SIG = 0;
 const int LED1 = 1;//for debug 1
 const int LED2 = 2;//for debug 2
 boolean mode = false;
-#define vibLimit 15000  //衝撃検知待ち時間
-#define chatterLimit 700 //衝撃検知一回の時間
+#define vibLength 15000  //衝撃検知待ち時間
+#define chatterLength 700 //衝撃検知一回の時間
 #define chatterTimes 10 //チャタリング回数
 
 void setup()
 {
   //入出力設定
-  pinMode(PHOTO_SW,INPUT);
-  pinMode(VIB_SW,INPUT);
-  pinMode(LED_SIG,OUTPUT);
-  pinMode(LED1,OUTPUT);
-  pinMode(LED2,OUTPUT);
-  digitalWrite(LED_SIG,LOW);
-  digitalWrite(LED1,LOW);
-  digitalWrite(LED2,LOW);
+  pinMode(PHOTO_SW, INPUT);
+  pinMode(VIB_SW, INPUT);
+  pinMode(LED_SIG, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  digitalWrite(LED_SIG, LOW);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
 }
 
 void loop()
@@ -31,21 +31,21 @@ void loop()
       break;
   }
 
-  digitalWrite(LED1,HIGH); //debug
+  digitalWrite(LED1, HIGH); //debug
   //衝撃検知
   boolean vibstate = false;
   int chattercount = 0;
   unsigned long vibtime;
   unsigned long chattertime;
   vibtime = millis(); //開始時間取得
-  while(millis()-vibtime<vibLimit)
+  while(millis() - vibtime < vibLength)
   {
     //チャタリングカウンタ
     if(vibstate != digitalRead(VIB_SW))
     {
       vibstate = !vibstate;
       chattercount++;
-      if(chattercount==1)
+      if(chattercount == 1)
         chattertime = millis();
     }
     //チャタリング回数が一定以上になればモード変更して脱出
@@ -56,7 +56,7 @@ void loop()
       break;
     }
     //チャタリング回数に達しなければカウンタ初期化
-    if(millis()-chattertime > chatterLimit)
+    if(millis() - chattertime > chatterLength)
       chattercount = 0;
   }
 
@@ -64,15 +64,15 @@ void loop()
   if (mode == true)
   {
     //変色
-  	digitalWrite(LED2,HIGH);
+  	digitalWrite(LED2, HIGH);
   	delay(1000);
-  	digitalWrite(LED2,LOW);
+  	digitalWrite(LED2, LOW);
   }
   else
   {
     //戻る
-  	digitalWrite(LED1,LOW);//衝撃検知終了
-  	digitalWrite(LED2,LOW);
+  	digitalWrite(LED1, LOW);//衝撃検知終了
+  	digitalWrite(LED2, LOW);
   	delay(1000);
   }
   //DEBUG
