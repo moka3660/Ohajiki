8cb60007907a3a9ea67f668deff87c8e8a9c3968
moka3660 <moka12almukha30@gmail.com>
2018-10-28 23:10:18 +0900

    update & fix LEDtest_UNO
    UNOじゃないけど！！！
    1)PWMピンのpinModeを削除
    2)loop関数内変更(テストわかりやすいように)

--- LEDtest_UNO/LEDtest_UNO.ino
+++ LEDtest_UNO/LEDtest_UNO.ino
@@ -8,11 +8,11 @@ const int LED_B = 5;
 
 void setup()
 {
-  pinMode(LED_SIG, OUTPUT);
+//  pinMode(LED_SIG, OUTPUT);
   pinMode(LED_R, OUTPUT);
   pinMode(LED_G, OUTPUT);
   pinMode(LED_B, OUTPUT);
-  digitalWrite(LED_SIG, LOW);
+//  digitalWrite(LED_SIG, LOW);
   digitalWrite(LED_R,LOW);
   digitalWrite(LED_G,LOW);
   digitalWrite(LED_B,LOW);
@@ -21,7 +21,10 @@ void setup()
 void loop()
 {
 	lighting(1000, 255, 255, 255);
-	lighting(1000, 255, 126, 0);
+	lighting(1000, 0, 0, 0);
+  lighting(500, 255, 0, 0);
+  lighting(500, 0, 255, 0);
+  lighting(500, 0, 0, 255);
 }
 
 
